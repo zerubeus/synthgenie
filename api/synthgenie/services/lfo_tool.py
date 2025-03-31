9e9636957d9e0277681932aab5c0a37637250798
zerubeus <zerubeus@gmail.com>
2025-03-31 15:31:07 +0200

    refactor: enhance LFO control methods in lfo_tool to utilize SynthControllerDeps for improved dependency management and add support for LFO3 functions

--- api/synthgenie/services/lfo_tool.py
+++ api/synthgenie/services/lfo_tool.py
@@ -1,92 +1,225 @@
-from services.synth_controller import BaseSynthController
-from data.digitone_params import digitone_config
+from pydantic_ai import RunContext
+from services.synth_controller import SynthControllerDeps
 
 import logging
 
 logger = logging.getLogger(__name__)
 
-lfo1_synth_controller = BaseSynthController(digitone_config.lfo.lfo_groups["lfo_1"])
-
-lfo2_synth_controller = BaseSynthController(digitone_config.lfo.lfo_groups["lfo_2"])
-
 
 # LFO1 Functions
-def set_lfo1_speed(value, midi_channel: int) -> bool:
+def set_lfo1_speed(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the speed of LFO1."""
-    return lfo1_synth_controller.get_direct_parameter("SPD", value, midi_channel)
+    return ctx.deps.lfo1_synth_controller.get_direct_parameter(
+        "SPD", value, midi_channel
+    )
 
 
-def set_lfo1_multiplier(value, midi_channel: int) -> bool:
+def set_lfo1_multiplier(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the multiplier of LFO1."""
-    return lfo1_synth_controller.get_direct_parameter("MULT", value, midi_channel)
+    return ctx.deps.lfo1_synth_controller.get_direct_parameter(
+        "MULT", value, midi_channel
+    )
 
 
-def set_lfo1_fade(value, midi_channel: int) -> bool:
+def set_lfo1_fade(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the fade in/out of LFO1."""
-    return lfo1_synth_controller.get_direct_parameter("FADE", value, midi_channel)
+    return ctx.deps.lfo1_synth_controller.get_direct_parameter(
+        "FADE", value, midi_channel
+    )
 
 
-def set_lfo1_destination(value, midi_channel: int) -> bool:
+def set_lfo1_destination(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the destination of LFO1."""
-    return lfo1_synth_controller.get_direct_parameter("DEST", value, midi_channel)
+    return ctx.deps.lfo1_synth_controller.get_direct_parameter(
+        "DEST", value, midi_channel
+    )
 
 
-def set_lfo1_waveform(value, midi_channel: int) -> bool:
+def set_lfo1_waveform(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the waveform of LFO1."""
-    return lfo1_synth_controller.get_direct_parameter("WAVE", value, midi_channel)
+    return ctx.deps.lfo1_synth_controller.get_direct_parameter(
+        "WAVE", value, midi_channel
+    )
 
 
-def set_lfo1_start_phase(value, midi_channel: int) -> bool:
+def set_lfo1_start_phase(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the start phase of LFO1."""
-    return lfo1_synth_controller.get_direct_parameter("SPH", value, midi_channel)
+    return ctx.deps.lfo1_synth_controller.get_direct_parameter(
+        "SPH", value, midi_channel
+    )
 
 
-def set_lfo1_trigger_mode(value, midi_channel: int) -> bool:
+def set_lfo1_trigger_mode(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the trigger mode of LFO1."""
-    return lfo1_synth_controller.get_direct_parameter("MODE", value, midi_channel)
+    return ctx.deps.lfo1_synth_controller.get_direct_parameter(
+        "MODE", value, midi_channel
+    )
 
 
-def set_lfo1_depth(value, midi_channel: int) -> bool:
+def set_lfo1_depth(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the depth of LFO1."""
-    return lfo1_synth_controller.get_direct_parameter("DEP", value, midi_channel)
+    return ctx.deps.lfo1_synth_controller.get_direct_parameter(
+        "DEP", value, midi_channel
+    )
 
 
 # LFO2 Functions
-def set_lfo2_speed(value, midi_channel: int) -> bool:
+def set_lfo2_speed(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the speed of LFO2."""
-    return lfo2_synth_controller.get_direct_parameter("SPD", value, midi_channel)
+    return ctx.deps.lfo2_synth_controller.get_direct_parameter(
+        "SPD", value, midi_channel
+    )
 
 
-def set_lfo2_multiplier(value, midi_channel: int) -> bool:
+def set_lfo2_multiplier(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the multiplier of LFO2."""
-    return lfo2_synth_controller.get_direct_parameter("MULT", value, midi_channel)
+    return ctx.deps.lfo2_synth_controller.get_direct_parameter(
+        "MULT", value, midi_channel
+    )
 
 
-def set_lfo2_fade(value, midi_channel: int) -> bool:
+def set_lfo2_fade(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the fade in/out of LFO2."""
-    return lfo2_synth_controller.get_direct_parameter("FADE", value, midi_channel)
+    return ctx.deps.lfo2_synth_controller.get_direct_parameter(
+        "FADE", value, midi_channel
+    )
 
 
-def set_lfo2_destination(value, midi_channel: int) -> bool:
+def set_lfo2_destination(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the destination of LFO2."""
-    return lfo2_synth_controller.get_direct_parameter("DEST", value, midi_channel)
+    return ctx.deps.lfo2_synth_controller.get_direct_parameter(
+        "DEST", value, midi_channel
+    )
 
 
-def set_lfo2_waveform(value, midi_channel: int) -> bool:
+def set_lfo2_waveform(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the waveform of LFO2."""
-    return lfo2_synth_controller.get_direct_parameter("WAVE", value, midi_channel)
+    return ctx.deps.lfo2_synth_controller.get_direct_parameter(
+        "WAVE", value, midi_channel
+    )
 
 
-def set_lfo2_start_phase(value, midi_channel: int) -> bool:
+def set_lfo2_start_phase(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the start phase of LFO2."""
-    return lfo2_synth_controller.get_direct_parameter("SPH", value, midi_channel)
+    return ctx.deps.lfo2_synth_controller.get_direct_parameter(
+        "SPH", value, midi_channel
+    )
 
 
-def set_lfo2_trigger_mode(value, midi_channel: int) -> bool:
+def set_lfo2_trigger_mode(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the trigger mode of LFO2."""
-    return lfo2_synth_controller.get_direct_parameter("MODE", value, midi_channel)
+    return ctx.deps.lfo2_synth_controller.get_direct_parameter(
+        "MODE", value, midi_channel
+    )
 
 
-def set_lfo2_depth(value, midi_channel: int) -> bool:
+def set_lfo2_depth(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
     """Set the depth of LFO2."""
-    return lfo2_synth_controller.get_direct_parameter("DEP", value, midi_channel)
+    return ctx.deps.lfo2_synth_controller.get_direct_parameter(
+        "DEP", value, midi_channel
+    )
+
+
+# LFO3 Functions
+def set_lfo3_speed(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
+    """Set the speed of LFO3."""
+    return ctx.deps.lfo3_synth_controller.get_direct_parameter(
+        "SPD", value, midi_channel
+    )
+
+
+def set_lfo3_multiplier(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
+    """Set the multiplier of LFO3."""
+    return ctx.deps.lfo3_synth_controller.get_direct_parameter(
+        "MULT", value, midi_channel
+    )
+
+
+def set_lfo3_fade(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
+    """Set the fade in/out of LFO3."""
+    return ctx.deps.lfo3_synth_controller.get_direct_parameter(
+        "FADE", value, midi_channel
+    )
+
+
+def set_lfo3_destination(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
+    """Set the destination of LFO3."""
+    return ctx.deps.lfo3_synth_controller.get_direct_parameter(
+        "DEST", value, midi_channel
+    )
+
+
+def set_lfo3_waveform(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
+    """Set the waveform of LFO3."""
+    return ctx.deps.lfo3_synth_controller.get_direct_parameter(
+        "WAVE", value, midi_channel
+    )
+
+
+def set_lfo3_start_phase(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
+    """Set the start phase of LFO3."""
+    return ctx.deps.lfo3_synth_controller.get_direct_parameter(
+        "SPH", value, midi_channel
+    )
+
+
+def set_lfo3_trigger_mode(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
+    """Set the trigger mode of LFO3."""
+    return ctx.deps.lfo3_synth_controller.get_direct_parameter(
+        "MODE", value, midi_channel
+    )
+
+
+def set_lfo3_depth(
+    ctx: RunContext[SynthControllerDeps], value: int, midi_channel: int
+) -> bool:
+    """Set the depth of LFO3."""
+    return ctx.deps.lfo3_synth_controller.get_direct_parameter(
+        "DEP", value, midi_channel
+    )
